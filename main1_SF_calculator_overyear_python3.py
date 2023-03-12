24b561d062f8763e06845e7f56f39f154065fbe7
noayarae <enoay7@yahoo.com>
2023-03-12 01:21:43 -0600

    Add files via upload

--- main1_SF_calculator_overyear_python3.py
+++ main1_SF_calculator_overyear_python3.py
@@ -1,11 +1,15 @@
 ### This code compute the Shade-Factor for streams
 
 ### Input
-# - "in01_topog_angle.csv":        CSV file with topographic angles for 0, 45, 90, 135, 270, 225, 270, 315 axis
+# - "in01_topog_angle.csv":        CSV file with topographic angles for azimuth: 0, 45, 90, 135, 270, 225, 270, 315. Zero for the North
 # - "in02_stream_data_LM_RM.csv":  CSV file with stream data: (0)number_of_sb,(1)longitude_rch,(2)latitude_rch, (3)tree_height_right_bank, (4)tree_hight_left_bank, (5)strm_azimute, (6)strm_width, (7)strm_slength
 # - "in03_SR_data_2019.csv":       CSV file with daily Solar Radiation: (0)Date, (1)julian_day, (2)Solar_Radiation
-# - Set time step "t_step" at L.150. It is suggested to set 0.01
-# - Set riparian "gap" at L.147      It is suggested to set 5
+# - Set time step "t_step" at around L.167. It is suggested to set 0.01
+# - Set riparian "gap" at around L.142      Here, I set 5 meters
+# - Set 'list_sbs' at aroud L.131:          with the list of sub-basins. If you want to run this for all the sun-basins, set a list with their number. For instance,
+# if you watershed has 60 sub-basins, set:list_sbs = list(range(0, n_sbs)) 
+# if you want to run only for sub-basins 21 and 26, set: list_sbs = [20, 25] 
+
 
 ### Output
 # - "sf001_top.csv" file with shade factor by topography
